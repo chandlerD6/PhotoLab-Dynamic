b55b15278a9b1bb276cfea69fe180488b1535111
Chandler Ditolla <chandlerditolla6@gmail.com>
2016-11-28 18:16:14 -0800

    Create Constants.h

---
+++ Constants.h
@@ -0,0 +1,22 @@
+#ifndef CONSTANTS_H_INCLUDED_
+#define CONSTANTS_H_INCLUDED_
+
+/* Do not modify it */
+
+/*** global definitions ***/
+#define SLEN    80      /* maximum length of file names and string*/
+#define ZOOM_FACTOR 2   /* Zooming factor for the zoom function */
+
+#define MAX_PIXEL 255
+#define SUCCESS 0
+#define EXIT 18
+
+/* used by overlay function */
+#define OVERLAY_DIFF 5
+
+/* metal color R/G/B */
+#define METAL_R 224
+#define METAL_G 223
+#define METAL_B 219
+
+#endif
